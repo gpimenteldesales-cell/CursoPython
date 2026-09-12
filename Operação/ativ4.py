12eb0115ba97952ca728c3ea7b9e55751b5a87b1
GUIMê <gpimenteldesales@gmail.com>
2026-09-12 13:40:12 -0300

    AtividadeDeMédias

--- Operação/ativ4.py
+++ Operação/ativ4.py
@@ -1,6 +1,7 @@
-nome="Joao"
-n1=7
-n2=9
-n3=5    
+nome=input("Digite seu nome: ")
+n1=int(input("Digite a nota do primeiro trimestre: "))
+n2=int(input("Digite a nota do segundo trimestre: "))
+n3=int(input("Digite a nota do terceiro trimestre: "))
 med=(n1+n2+n3)//3
-print ("A média de",nome,"é:",med)
+print("Nome: ",nome)
+print("Média do ano: ",med)
