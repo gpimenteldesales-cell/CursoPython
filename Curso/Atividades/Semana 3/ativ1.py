89bac80885b1ab26ebcfd0b8f3c14d2ca7b19de7
GUIMê <gpimenteldesales@gmail.com>
2026-09-26 10:49:55 -0300

    OrganizationFolders

---
+++ Curso/Atividades/Semana 3/ativ1.py
@@ -0,0 +1,42 @@
+# # Checar Maioridade
+# idade = int(input("Digite sua idade: \n"))
+
+# if idade >= 18:
+#     print("Acesso Autorizado")
+# else:
+#     print("Acesso não autorizado")
+
+# # Verificar Número Positivo
+# num = int (input("Digite um número inteiro: \n"))
+
+# if num >= 0:
+#     print("Número é positivo")
+# else:
+#     print("Número é negativo")
+    
+# # Autenticação de Senha
+# pin = 1234
+# inp = int(input("Digite a o PIN\n"))
+
+# if inp == pin:
+#     print("Login realizado com sucesso.")
+# else:
+#     print("Saia!!")
+
+# Pesquisa em Lista
+frutas = ["Maçã", "Banana", "Laranja"]
+escolhaFrutas = input(f"{frutas}\nEscolha um produto do estoque:\n")
+
+
+if escolhaFrutas.lower() in frutas:
+    print("Fruta disponivel no estoque.")
+else:
+    print("Fruta indisponivel no estoque.")
+    
+# Confirmação de Ação
+sim = input("Digite 'sim' para receber uma notificação\n")
+
+if sim == "sim":
+    print ("Enviando notificação...\n")
+else:
+    print ("Não notificado.\n")
